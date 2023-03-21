6b85c736ba197fbf624d336a9badd8cfa7f61722
Josef Veselý <49988965+JosefVesely@users.noreply.github.com>
2023-03-21 22:52:20 +0100

    Add files via upload

--- pong.py
+++ pong.py
@@ -1,16 +1,24 @@
 import pygame
 import random
+from tkinter import *
+from tkinter import messagebox
 
 # Nastavení
 pygame.init()
 pygame.display.set_caption("Pong")
+Tk().wm_withdraw() # schová hlavní okno tkinter
 
+# Konstanty
 OKNO_SIRKA = 800
 OKNO_VYSKA = 600
+MAX_BODY = 5
+MIC_ZACATECNI_RYCHLOST = 2
+BARVA_BILA = (255, 255, 255)
+BARVA_MODRA = (0, 0, 120)
 
 screen = pygame.display.set_mode((OKNO_SIRKA, OKNO_VYSKA))
 clock = pygame.time.Clock()
-font = pygame.font.Font(None, 50)
+font = pygame.font.Font(None, 100)
 
 # HRÁČI
 hrac_vyska = 100
@@ -31,7 +39,7 @@ hrac2_skore = 0
 mic_x = 390
 mic_y = 290
 mic_velikost = 20
-mic_rychlost = 2
+mic_rychlost = MIC_ZACATECNI_RYCHLOST
 mic_smer_x = random.randint(0, 1) # 0 - doleva 1 - doprava  
 mic_smer_y = random.randint(0, 1) # 0 - dolu   1 - nahoru   
 
@@ -43,8 +51,8 @@ while True:
     clock.tick(60)
 
     # Nakresli pozadí
-    screen.fill((0, 0, 0))
-    pygame.draw.line(screen, (255, 255, 255), (400, 0), (400, 600)) # Půlící čára
+    screen.fill(BARVA_MODRA)
+    pygame.draw.line(screen, BARVA_BILA, (400, 0), (400, 600), 2) # Půlící čára
 
     # VSTUP
     for event in pygame.event.get():
@@ -108,43 +116,53 @@ while True:
         hrac2_y = OKNO_VYSKA - hrac_vyska
 
     # Padl gól? Přidej skóre
-    if mic_x > OKNO_SIRKA - mic_velikost: # Pravá strana
+    if mic_x > OKNO_SIRKA: # Pravá strana
         hrac1_skore += 1
+        # Resetuj hráče
         hrac1_y = 250
         hrac2_y = 250
+        # Resetuj míč
         mic_smer_y = random.randint(0, 1)
-        mic_rychlost = 2
+        mic_rychlost = MIC_ZACATECNI_RYCHLOST
         mic_x = 390
         mic_y = 290
         
-    elif mic_x < 0: # Levá strana
+    elif mic_x + mic_velikost < 0: # Levá strana
         hrac2_skore += 1
+        # Resetuj hráče
         hrac1_y = 250
         hrac2_y = 250
+        # Resetuj míč
         mic_smer_y = random.randint(0, 1)
-        mic_rychlost = 2
+        mic_rychlost = MIC_ZACATECNI_RYCHLOST
         mic_x = 390
         mic_y = 290
 
-    # Resetuj skóre
-    if hrac1_skore > 10 or hrac2_skore > 10:
+    # Zjisti, jestli někdo vyhrál
+    if hrac1_skore == MAX_BODY:
+        messagebox.showinfo("Pong", f"{hrac1_skore} : {hrac2_skore}\nHráč 1 vyhrál!")
+        hrac1_skore = 0
+        hrac2_skore = 0
+
+    elif hrac2_skore == MAX_BODY:
+        messagebox.showinfo("Pong", f"{hrac1_skore} : {hrac2_skore}\nHráč 2 vyhrál!")
         hrac1_skore = 0
         hrac2_skore = 0
 
     # RENDER
 
     # Nakresli skóre
-    text = font.render(str(hrac1_skore), True, (255, 255, 255))
-    screen.blit(text, (330, 50))
-    text = font.render(str(hrac2_skore), True, (255, 255, 255))
-    screen.blit(text, (455, 50))
+    text = font.render(str(hrac1_skore), True, BARVA_BILA)
+    screen.blit(text, (180, 50))
+    text = font.render(str(hrac2_skore), True, BARVA_BILA)
+    screen.blit(text, (585, 50))
 
     # Nakresli hráče
-    pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(hrac1_x, hrac1_y, hrac_sirka, hrac_vyska))
-    pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(hrac2_x, hrac2_y, hrac_sirka, hrac_vyska))
+    pygame.draw.rect(screen, BARVA_BILA, pygame.Rect(hrac1_x, hrac1_y, hrac_sirka, hrac_vyska))
+    pygame.draw.rect(screen, BARVA_BILA, pygame.Rect(hrac2_x, hrac2_y, hrac_sirka, hrac_vyska))
 
     # Nakresli míč
-    pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(mic_x, mic_y, mic_velikost, mic_velikost))
+    pygame.draw.rect(screen, BARVA_BILA, pygame.Rect(mic_x, mic_y, mic_velikost, mic_velikost))
 
     pygame.display.update()
 
